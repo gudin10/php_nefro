164e73c54b4c6fed372733b4c7d1345ff1b9891f
gudin10 <carlosandresgudin@gmail.com>
2022-07-27 00:19:02 -0500

    expor-base

--- reporte_nominal.php
+++ reporte_nominal.php
@@ -5,47 +5,35 @@ $dbconect = pg_connect("host=190.60.242.160 port=5432 dbname=clinica_desarrollo 
 
 
 
-function filtrarDatos(&$str){
+function limpiarData(&$str){
     $str = preg_replace("/\t/","\\t",$str);
     $str = preg_replace("/\r?\n/","\\n",$str);
     #if( strtr($str,'"') ) $str = '"'.str_replace('"','""',$str). '"';
 }
 
 //file
-$filename = "users-data_" .date('Y-m-d').".xls";
-//cols
-$headers= array('ID','NOMBRE');
-
-$excelData= implode("\t",array_values($headers)). "\n";
+$filename = "base_nominal_" .date('Y-m-d').".xls";
 
 $query="SELECT id,nompersonal FROM adm.personal limit 10";
 
-$consulta=pg_query($dbconect,$query);
+$consulta=pg_query($dbconect,$query) or die('Fallo consulta');
 #$query = db->consulta("SELECT * from adm.personal limit 10");
-if($consulta){
-    if(pg_num_rows($consulta)>0){
-        
-        while($obj=pg_fetch_object($consulta)){
-            #$status=($obj->activo=1) ? 'Activo':'Inactivo';
-            echo "<br>Listado de personal<br>";
-        
-            echo "$obj->id</br>";
-            echo "$obj->nompersonal</br>";
-
-            $lineData= array($obj->id,$obj->nompersonal);
-            array_walk($lineData,'filtrarDatos');
-
-            $excelData .=implode("\t",array_values($lineData)). "\n";
-        }
-    }
-}else{
-    echo "No se encontro datos";
-}
-
-//headers para descarga
 header("Content-Type: application/vnd.ms-excel");
 header("Content-Disposition: attachment; filename=\"$filename\"");
+$flag=false;
+
+while(false !== ($row = pg_fetch_assoc($consulta))) {
+    if(!$flag) {
+      // display field/column names as first row
+      echo implode("\t", array_keys($row)) . "\r\n";
+      $flag = true;
+    }
+    array_walk($row, 'limpiarData');
+    echo implode("\t", array_values($row)) . "\r\n";
+    }
+
+//headers para descarga
+
 
 #echo $excelData;
-exit;
 ?>
