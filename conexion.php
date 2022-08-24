daca7325fa15a8fb6b0b0a280a5f442694056a03
Firmas Digitales <soporte01.fd@gmail.com>
2022-08-24 17:00:47 -0400

    base_nominal

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php 
 //require_once('connections/pgconn.php');
 
-$dbconect = pg_connect("host=190.60.243.121 port=5432 dbname=clinica_produccion user=postgres password=CRIS-PRO2021** options='--client_encoding=UTF8'");
+$dbconect = pg_connect("host=192.168.125.77 port=5432 dbname=clinica_produccion user=postgres password=CRIS-PRO2021** options='--client_encoding=UTF8'");
 
 #$query="SELECT * FROM adm.personal limit 10";
 
